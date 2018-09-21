e3c7ebce5609262be89e5b1987d6eb8ea28848af
hayahisatsuyoshi <hayahisatsuyoshi@172.20.87.39>
2018-09-21 16:53:18 +0900

    ０９２１

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -40,36 +40,28 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//doGet(request, response);
-		System.out.println("login.java");
+		//doGet(request, response)
 		HttpSession session = request.getSession();	//セッション
 		String path = null;	//遷移用path
+		int len; //user_noのlength
 
 		int user_no = Integer.parseInt(request.getParameter("user_number"));	//ユーザ番号
 		String user_pass = request.getParameter("user_pass"); 	//ユーザパス
-
-		System.out.println(user_no);
-		System.out.println(user_pass);
 
 		//LoginDao
 		LoginDao ldao = new LoginDao();
 		UserPassBean userpassbean = ldao.User_loginDao(user_no);
 
 		if(ldao.User_loginDao(user_no) == null){
-			System.out.println("e1");
 			path = "/login.jsp";
 		}else if(userpassbean.getUser_number() ==  user_no && userpassbean.getUser_pass().equals(user_pass)){
 			session.setAttribute("user_number",user_no);	//セッションにユーザナンバーを確認
 			path = "WEB-INF/jsp/main01.jsp";
-			System.out.println("e2");
 		}else{
 			path = "/login.jsp";
-			System.out.println("e3");
 		}
-		System.out.println(path);
 		request.getRequestDispatcher(path).forward(request, response);
 
-		System.out.println("aiueo");
 	}
 
 }
