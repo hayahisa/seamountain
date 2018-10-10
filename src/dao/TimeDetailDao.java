45b153168f68cd76f9ce58f7a93bfa9bd1317675
hayahisatsuyoshi <hayahisatsuyoshi@hayahisano-macbook-puro.local>
2018-10-10 15:18:16 +0900

    １０１０時間割表示のBEANを作成

--- src/dao/TimeDetailDao.java
+++ src/dao/TimeDetailDao.java
@@ -1,6 +1,83 @@
 package dao;
 
+import model.RoomIdBean;
+import model.SubjectIdBean;
 
-public class TimeDetailDao {
+public class TimeDetailDao extends DaoBase{
+
+	//subject_idを取り出す
+	public SubjectIdBean Subject_id(int time_id){
+		SubjectIdBean subjectidbean = null;
+		try{
+			super.connection();
+			String sql = "SELECT td.one_subject_id,"
+					+ "td.two_subject_id,td."
+					+ "three_subject_id,td."
+					+ "four_subject_id, "
+					+ "td.day"
+					+ "FROM time_detail td"
+					+ "WHERE td.time_id = ?";
+
+			stmt.setInt(1, time_id);
+			rs = stmt.executeQuery();
+			rs.next();
+
+			//Beanに格納
+			subjectidbean = new SubjectIdBean(
+					rs.getString(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getString(5)
+					);
+		}catch (Exception e){
+			System.out.println(e);
+		}finally{
+			try{
+				super.DbClose();
+			}catch (Exception e){
+				System.out.println("error");
+			}
+		}
+		return subjectidbean;
+	}
+
+
+	//room_idを取り出す
+	public RoomIdBean Room_id(int time_id){
+		RoomIdBean roomidbean = null;
+		try{
+			super.connection();
+			String sql = "SELECT td.one_room_id,"
+					+ "td.two_room_id,"
+					+ "td.three_room_id,"
+					+ "td.four_room_id,"
+					+ "td.day"
+					+ "FROM time_detail td"
+					+ "WHERE td.time_id = ?";
+
+			stmt.setInt(1, time_id);
+			rs = stmt.executeQuery();
+			rs.next();
+
+			//Beanに格納
+			roomidbean = new RoomIdBean(
+					rs.getString(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getString(5)
+					);
+		}catch (Exception e){
+			System.out.println(e);
+		}finally{
+			try{
+				super.DbClose();
+			}catch (Exception e){
+				System.out.println("error");
+			}
+		}
+		return roomidbean;
+	}
 
 }
--- src/model/TimeTableBean.java
+++ src/model/TimeTableBean.java
@@ -5,9 +5,115 @@ import java.io.Serializable;
 public class TimeTableBean implements Serializable {
 	//時間割の表示に使用
 
-	private int timeId;
-	private String timeName;
-	private String day;
+	private int time_id;				//時間割ID
+	private String time_name;			//時間割名
+	private String one_room_name;		//１限目教室名
+	private String one_subject_name;	//１限目教科名
+	private String two_room_name;		//2限目教室名
+	private String two_subject_name;	//2限目教科名
+	private String three_room_name;		//3限目教室名
+	private String three_subject_name;	//3限目教科名
+	private String four_room_name;		//4限目教室名
+	private String four_subject_name;	//4限目教科名
 
+	public TimeTableBean() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public TimeTableBean(int time_id, String time_name, String one_room_name, String one_subject_name,
+			String two_room_name, String two_subject_name, String three_room_name, String three_subject_name,
+			String four_room_name, String four_subject_name) {
+		super();
+		this.time_id = time_id;
+		this.time_name = time_name;
+		this.one_room_name = one_room_name;
+		this.one_subject_name = one_subject_name;
+		this.two_room_name = two_room_name;
+		this.two_subject_name = two_subject_name;
+		this.three_room_name = three_room_name;
+		this.three_subject_name = three_subject_name;
+		this.four_room_name = four_room_name;
+		this.four_subject_name = four_subject_name;
+	}
+
+	public int getTime_id() {
+		return time_id;
+	}
+
+	public void setTime_id(int time_id) {
+		this.time_id = time_id;
+	}
+
+	public String getTime_name() {
+		return time_name;
+	}
+
+	public void setTime_name(String time_name) {
+		this.time_name = time_name;
+	}
+
+	public String getOne_room_name() {
+		return one_room_name;
+	}
+
+	public void setOne_room_name(String one_room_name) {
+		this.one_room_name = one_room_name;
+	}
+
+	public String getOne_subject_name() {
+		return one_subject_name;
+	}
+
+	public void setOne_subject_name(String one_subject_name) {
+		this.one_subject_name = one_subject_name;
+	}
+
+	public String getTwo_room_name() {
+		return two_room_name;
+	}
+
+	public void setTwo_room_name(String two_room_name) {
+		this.two_room_name = two_room_name;
+	}
+
+	public String getTwo_subject_name() {
+		return two_subject_name;
+	}
+
+	public void setTwo_subject_name(String two_subject_name) {
+		this.two_subject_name = two_subject_name;
+	}
+
+	public String getThree_room_name() {
+		return three_room_name;
+	}
+
+	public void setThree_room_name(String three_room_name) {
+		this.three_room_name = three_room_name;
+	}
+
+	public String getThree_subject_name() {
+		return three_subject_name;
+	}
+
+	public void setThree_subject_name(String three_subject_name) {
+		this.three_subject_name = three_subject_name;
+	}
+
+	public String getFour_room_name() {
+		return four_room_name;
+	}
+
+	public void setFour_room_name(String four_room_name) {
+		this.four_room_name = four_room_name;
+	}
+
+	public String getFour_subject_name() {
+		return four_subject_name;
+	}
+
+	public void setFour_subject_name(String four_subject_name) {
+		this.four_subject_name = four_subject_name;
+	}
 
 }
