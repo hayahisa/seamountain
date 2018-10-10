31d59b134c0ab2d26f5e8922b17653a0d9f8b590
hayahisatsuyoshi <hayahisatsuyoshi@hayahisano-macbook-puro.local>
2018-10-10 13:06:35 +0900

    クェcg

--- src/model/TimeTable.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class TimeTable {
-	//時間割の表示に使用
-
-}
---
+++ src/model/TimeTableBean.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.io.Serializable;
+
+public class TimeTableBean implements Serializable {
+	//時間割の表示に使用
+
+	private int timeId;
+	private String timeName;
+	private String day;
+
+
+}
