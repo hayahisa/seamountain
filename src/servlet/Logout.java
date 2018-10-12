a4683367c33c3cc5d6ee9249fb02aa7406aae722
nky-1 <nky-1@LAPTOP-TAP1EUI7>
2018-10-12 15:14:34 +0900

    退会

--- src/servlet/Logout.java
+++ src/servlet/Logout.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Logout
@@ -28,7 +29,6 @@ public class Logout extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("ll");
 		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
@@ -37,7 +37,10 @@ public class Logout extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+		
+		HttpSession session = request.getSession(false);
+		session.invalidate();
+		
 		String path = "WEB-INF/jsp/logout.jsp";
 
 		request.getRequestDispatcher(path).forward(request, response);
