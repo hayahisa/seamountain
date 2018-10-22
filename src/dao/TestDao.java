a53060b70e6f076d5e1c0be48c9f3a6a060cf4c4
hayahisatsuyoshi <hayahisatsuyoshi@172.20.87.240>
2018-10-22 09:52:03 +0900

    1022_1

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestDao")
 public class TestDao extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,6 +37,7 @@ public class TestDao extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		//test
 	}
 
 }
